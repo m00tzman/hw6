7f4a533ab75d423bba6882f40549bc65e24feeec
Cory Rabiea <m00tzmanAlmighty@gmail.com>
2015-04-16 17:51:55 -0400

    made delete and post functions. posts not working. site is still nasty looking

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,5 +5,29 @@ class PostsController < ApplicationController
 	def show
 		@post = Post.find_by_id(params[:id])
 	end
+	def new
+		@post = Post.new
+	end
+	def create
+		@post = Post.new(params[:post])
+
+		if @post.save
+			redirect_to post_path(@post), notice: "You added a new post!"
+		else
+			flash[:alert] = "There was an error creating the post"
+			render :new
+		end
+	end
+
+	def destroy
+		@post = Post.find_by_id(params[:id])
+
+		if @post.destroy
+			flash[:notice] = "You destroyed a post!"
+		else
+			flash[:alert] = "There was an error while attempting to delete the post"
+		end
+		redirect_to posts_path
+	end
 end
 
